b652f91900884071ae3a596d61c0b422633f1b6c
Your Name <you@example.com>
2024-10-17 12:14:21 +0200

    feat: fix to deploy

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 import { pokemon, pokemonTypes } from "./data/pokemon.js";
 import { cardElGen, cardListGen, buttonGen } from "./modules/components.js";
 
-// Inizializzazione della sezione principale e del pulsante
 const mainSectionEl = document.querySelector(".main");
 const btnAdd = document.querySelector(".btn-add");
 const headerElement = document.querySelector(".header");
 
-// Funzione per generare un nuovo Pokémon
 const generateNewPokemon = (id) => {
   return {
     id,
-    name: `Pokemon ${id}`, // Nome generico per esempio
-    type: pokemonTypes[Math.floor(Math.random() * pokemonTypes.length)], // Seleziona un tipo casuale
-    image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`, // URL dell'immagine (assicurati che esistano)
+    name: `Pokemon ${id}`,
+    type: pokemonTypes[Math.floor(Math.random() * pokemonTypes.length)],
+    image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
   };
 };
 
-// Funzione per rendere la lista dei tipi di Pokémon
 const renderListTypePokemon = () => {
   pokemonTypes.forEach((type) => {
     const btnType = buttonGen(`Type - ${type}`, type);
@@ -26,37 +23,35 @@ const renderListTypePokemon = () => {
         (pokemonsingolo) => pokemonsingolo.type === this.id
       );
 
-      renderFilteredList(filteredPokemon); // Rendi la lista filtrata
+      renderFilteredList(filteredPokemon);
     });
 
     headerElement.append(btnType);
   });
 };
 
-// Funzione per rendere la lista di card Pokémon
 const renderListCard = () => {
-  mainSectionEl.innerHTML = ""; // Pulisci la sezione principale
+  mainSectionEl.innerHTML = "";
 
-  const cardList = cardListGen(); // Crea un nuovo contenitore per le card
+  const cardList = cardListGen();
 
   pokemon.forEach((singlePokemon) => {
     const cardEl = cardElGen(singlePokemon);
     cardList.append(cardEl);
   });
 
-  mainSectionEl.append(cardList); // Aggiungi la lista di card alla sezione principale
+  mainSectionEl.append(cardList);
 };
 
-// Funzione per rendere la lista filtrata di card Pokémon
 const renderFilteredList = (filteredPokemon) => {
-  mainSectionEl.innerHTML = ""; // Pulisci la sezione principale
+  mainSectionEl.innerHTML = "";
 
-  const cardList = cardListGen(); // Crea un nuovo contenitore per le card
+  const cardList = cardListGen();
 
   if (filteredPokemon.length === 0) {
     const noPokemonMessage = document.createElement("p");
     noPokemonMessage.textContent = "Non esistono pokemon con questo tipo";
-    mainSectionEl.append(noPokemonMessage); // Aggiungi un messaggio se non ci sono Pokémon
+    mainSectionEl.append(noPokemonMessage);
     return;
   }
 
@@ -65,27 +60,24 @@ const renderFilteredList = (filteredPokemon) => {
     cardList.append(cardEl);
   });
 
-  mainSectionEl.append(cardList); // Aggiungi la lista filtrata alla sezione principale
+  mainSectionEl.append(cardList);
 };
 
-// Gestore per l'evento di aggiunta di un Pokémon
 btnAdd.addEventListener("click", function () {
-  const newId = pokemon.length + 1; // Genera un nuovo ID per il Pokémon
-  const newPokemon = generateNewPokemon(newId); // Crea un nuovo Pokémon
+  const newId = pokemon.length + 1;
+  const newPokemon = generateNewPokemon(newId);
 
-  // Controlla se il Pokémon esiste già
   const isEqual = pokemon.some((i) => i.id === newPokemon.id);
 
   if (isEqual) {
     console.log("Il pokemon esiste già!");
   } else {
-    pokemon.push(newPokemon); // Aggiungi il nuovo Pokémon all'array
-    renderListCard(); // Rigenera la lista di card
+    pokemon.push(newPokemon);
+    renderListCard();
   }
 });
 
-// Inizializzazione dell'app
 window.onload = () => {
-  renderListTypePokemon(); // Rendi i bottoni per i tipi di Pokémon
-  renderListCard(); // Mostra la lista iniziale di card
+  renderListTypePokemon();
+  renderListCard();
 };
